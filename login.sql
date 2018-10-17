5e2e5db3fe4179a1534d1c4bd95de005edb5abef
quezan <quezanw@gmail.com>
2018-10-16 18:31:41 -0700

    added requirements.txt

--- login.sql
+++ login.sql
@@ -1,29 +1,70 @@
-create database loginDB;
-use loginDB;
-create table users (
-	id int auto_increment primary key,
-    first_name varchar(50),
-    last_name varchar(50),
-    email varchar(255),
-    password_hash varchar(255),
-    created_at datetime,
-    updated_at datetime
-);
+-- MySQL Workbench Forward Engineering
 
-create table friendships (
-	id int auto_increment primary key,
-    friend_1_id int,
-    friend_2_id int,
-    created_at datetime,
-    updated_at datetime
-);
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-create table messages(
-	id int auto_increment primary key,
-    user_id int,
-    user2_id int,
-    content text,
-    created_at datetime,
-    updated_at datetime
-);
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema loginDB
+-- -----------------------------------------------------
 
+-- -----------------------------------------------------
+-- Schema loginDB
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `loginDB` DEFAULT CHARACTER SET latin1 ;
+USE `loginDB` ;
+
+-- -----------------------------------------------------
+-- Table `loginDB`.`friendships`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `loginDB`.`friendships` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `friend_1_id` INT(11) NULL DEFAULT NULL,
+  `friend_2_id` INT(11) NULL DEFAULT NULL,
+  `created_at` DATETIME NULL DEFAULT NULL,
+  `updated_at` DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 17
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `loginDB`.`messages`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `loginDB`.`messages` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `user_id` INT(11) NULL DEFAULT NULL,
+  `user2_id` INT(11) NULL DEFAULT NULL,
+  `content` TEXT NULL DEFAULT NULL,
+  `created_at` DATETIME NULL DEFAULT NULL,
+  `updated_at` DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 19
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `loginDB`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `loginDB`.`users` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(50) NULL DEFAULT NULL,
+  `last_name` VARCHAR(50) NULL DEFAULT NULL,
+  `email` VARCHAR(255) NULL DEFAULT NULL,
+  `password_hash` VARCHAR(255) NULL DEFAULT NULL,
+  `created_at` DATETIME NULL DEFAULT NULL,
+  `updated_at` DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 25
+DEFAULT CHARACTER SET = latin1;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
